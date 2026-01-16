cafd70b889536c7aa9b3a89b3717fb4ea778cdeb
Janna007 <jannakondeth5@gmail.com>
2026-01-16 18:23:10 +0530

    Langchain interrupts

---
+++ AI_Agents/LangChain/HITL/data.js
@@ -0,0 +1,21 @@
+[
+  {
+    id: "980e9e6cce3ec10a1af373f00d32d71d",
+    value: {
+      actionRequests: [
+        {
+          name: "refund",
+          args: { emails: ["18c3f2a1b5d6e789", "18c3d5b8e1f3a456"] },
+          description:
+            'Refund pending approval\n\nTool: refund\nArgs: {\n  "emails": [\n    "18c3f2a1b5d6e789",\n    "18c3d5b8e1f3a456"\n  ]\n}',
+        },
+      ],
+      reviewConfigs: [
+        {
+          actionName: "refund",
+          allowedDecisions: ["approve", "edit", "reject"],
+        },
+      ],
+    },
+  },
+];
